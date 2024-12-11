3d5dff9b5e649c8cfe203eae3d22e93d1b5f6119
Rui Ferreira <147440895+Ruifsf@users.noreply.github.com>
2024-12-11 19:37:11 +0000

    feat: Initial skeleton for Taskify page

---
+++ src/views/Taskify.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+</script>
+<template>
+    <div>
+        <h1>Taskify page</h1>
+        <div class="column">
+            <div>Todo</div>
+            <div>Ongoing</div>
+            <div>Done</div>
+        </div>
+    </div>
+</template>
+
+<style>
+.column {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+</style>

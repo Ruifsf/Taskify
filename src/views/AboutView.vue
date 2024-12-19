bc0739b21128471acc0aef219aad13dabb8e9826
Rui Ferreira <147440895+Ruifsf@users.noreply.github.com>
2024-12-19 19:20:36 +0000

    feat: Added content to About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Who I am:</h1>
+    <h2>Name: Rui Ferreira</h2>
+    <h2>Age: 29</h2>
+    <h2>Country: Portugal</h2>
+    <h2>Experience: 4 years as Frontend Developer</h2>
+    <h2>Studies: Masters in Computer Engineering, University of Lisbon (<a
+        href="https://edurank.org/uni/university-of-lisbon/" target="_blank">Top 80 in Europe</a>)</h2>
+    <h2>Main Technologies: JavaScript, TypeScript, React, Vue</h2>
+    <h2>Hobbies: Climbing, Running</h2>
+    <h2>LinkedIn: <a href="https://linkedin.com/in/Ruifsferreira" target="_blank">linkedin.com/in/Ruifsferreira</a></h2>
+    <div class="spacer"></div>
+    <h1>About this Website:</h1>
+    <p>This is my first simple project with Vue (3).</p>
+    <p>The goal was to create a simple Task List in order to learn and practice Vue.</p>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.spacer {
+  height: 40px;
 }
 </style>

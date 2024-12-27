7032ce0d7a5d0abc751c9be05612fe41d3487ef2
Rui Ferreira <147440895+Ruifsf@users.noreply.github.com>
2024-12-27 18:08:56 +0000

    feat: changed About page style

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <h1>Who I am:</h1>
-    <h2>Name: Rui Ferreira</h2>
-    <h2>Age: 29</h2>
-    <h2>Country: Portugal</h2>
-    <h2>Experience: 4 years as Frontend Developer</h2>
-    <h2>Studies: Masters in Computer Engineering, University of Lisbon (<a
-        href="https://edurank.org/uni/university-of-lisbon/" target="_blank">Top 80 in Europe</a>)</h2>
-    <h2>Main Technologies: JavaScript, TypeScript, React, Vue</h2>
-    <h2>Hobbies: Climbing, Running</h2>
-    <h2>LinkedIn: <a href="https://linkedin.com/in/Ruifsferreira" target="_blank">linkedin.com/in/Ruifsferreira</a></h2>
+    <p><b>Name:</b> Rui Ferreira</p>
+    <p><b>Age:</b> 29</p>
+    <p><b>Country:</b> Portugal</p>
+    <p><b>Experience:</b> 4 years as Frontend Developer</p>
+    <p><b>Studies:</b> Masters in Computer Engineering, University of Lisbon (<a
+        href="https://edurank.org/uni/university-of-lisbon/" target="_blank">Top 80 in Europe</a>)</p>
+    <p><b>Main Technologies:</b> JavaScript, TypeScript, React, Vue</p>
+    <p><b>Hobbies:</b> Climbing, Running</p>
+    <p><b>LinkedIn:</b> <a href="https://linkedin.com/in/Ruifsferreira"
+        target="_blank">linkedin.com/in/Ruifsferreira</a></p>
     <div class="spacer"></div>
-    <h1>About this Website:</h1>
+    <h2>About this Website:</h2>
     <p>This is my first simple project with Vue (3).</p>
     <p>The goal was to create a simple Task List in order to learn and practice Vue.</p>
   </div>

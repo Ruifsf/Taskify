f57ea4978658cc546f6be4e6194000a2701f684e
Rui Ferreira <147440895+Ruifsf@users.noreply.github.com>
2024-12-27 19:28:07 +0000

    feat: added animation to text in About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,33 @@
+<script setup lang="ts">
+import Typed from 'typed.js';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.querySelectorAll('.typed').forEach(function (el) {
+    new Typed(el, {
+      strings: el.getAttribute('data-content') ? [el.getAttribute('data-content')] : [''],
+      typeSpeed: 25,
+      showCursor: false,
+    })
+  });
+})
+
+
+</script>
+
 <template>
   <div>
     <h1>Who I am:</h1>
-    <p><b>Name:</b> Rui Ferreira</p>
-    <p><b>Age:</b> 29</p>
-    <p><b>Country:</b> Portugal</p>
-    <p><b>Experience:</b> 4 years as Frontend Developer</p>
-    <p><b>Studies:</b> Masters in Computer Engineering, University of Lisbon (<a
-        href="https://edurank.org/uni/university-of-lisbon/" target="_blank">Top 80 in Europe</a>)</p>
-    <p><b>Main Technologies:</b> JavaScript, TypeScript, React, Vue</p>
-    <p><b>Hobbies:</b> Climbing, Running</p>
-    <p><b>LinkedIn:</b> <a href="https://linkedin.com/in/Ruifsferreira"
-        target="_blank">linkedin.com/in/Ruifsferreira</a></p>
+    <p><b>Name:</b> <span class="typed" data-content="Rui Ferreira"></span></p>
+    <p><b>Age:</b> <span class="typed" data-content="29"></span></p>
+    <p><b>Country:</b> <span class="typed" data-content="Portugal"></span></p>
+    <p><b>Experience:</b> <span class="typed" data-content="4 years as Frontend Developer"></span></p>
+    <p><b>Studies:</b> <span class="typed" data-content="Masters in Computer Engineering, University of Lisbon `(<a
+        href='https://edurank.org/uni/university-of-lisbon/' target='_blank'>Top 80 in Europe</a>)` "></span></p>
+    <p><b>Main Technologies:</b> <span class="typed" data-content="JavaScript, TypeScript, React, Vue"></span></p>
+    <p><b>Hobbies:</b> <span class="typed" data-content="Climbing, Running"></span></p>
+    <p><b>LinkedIn:</b> <span class="typed" data-content="`<a href='https://linkedin.com/in/Ruifsferreira'
+        target='_blank'>linkedin.com/in/Ruifsferreira</a>`"></span></p>
     <div class="spacer"></div>
     <h2>About this Website:</h2>
     <p>This is my first simple project with Vue (3).</p>
